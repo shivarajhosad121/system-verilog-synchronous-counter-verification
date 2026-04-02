4249012d08ac0143e37bba828fc25362efdf1803
shivarajhosad121 <shivarajhosad121@gmail.com>
2026-04-02 18:20:36 +0530

    Create counter-sync.sv

---
+++ rtl/counter-sync.sv
@@ -0,0 +1,15 @@
+module counter_sync #(parameter WIDTH = 4)(
+    input  logic clk,
+    input  logic rst,
+    input  logic en,
+    output logic [WIDTH-1:0] count
+);
+
+always_ff @(posedge clk) begin
+    if (rst)
+        count <= '0;
+    else if (en)
+        count <= count + 1;
+end
+
+endmodule
